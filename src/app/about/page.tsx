43f5d4746e615a25ff853cc053dc3084eb7d4b5e
Salgado3 <jsalg10@gmail.com>
2025-08-12 10:25:45 -0500

    update single quote to &apos;

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,11 +29,11 @@ const page = () => {
       </div>
       <p className={styles.text}>Every place tells a story.</p>
       <p style={{ textAlign: "justify" }}>
-        Whether it's found in cobblestone streets, the endless alleys, the
+        Whether it&apos;s found in cobblestone streets, the endless alleys, the
         homemade food and winding markets or the people hanging out in the town
         square. My hope is that this site will inspire you to plan that trip
-        you've been holding off on, pack your bags, and add the next chapter to
-        your story...
+        you&apos;ve been holding off on, pack your bags, and add the next
+        chapter to your story...
       </p>
     </Card>
   );
